42cb02f3df56ca3df54b4274d9fc0acb455f1236
koodikuningas <34175767+koodikuningas@users.noreply.github.com>
2017-12-04 15:08:06 +0200

    Added part 2 solution to Day 2

--- Day2.java
+++ Day2.java
@@ -11,56 +11,84 @@ import java.util.List;
 
 class SpreadSheet
 {
-	private List<String> lines;
+    private List<String> lines;
 
-	void readFromFile(String fileName)
-	{
-		try 
-		{
-			Path path = Paths.get(".", fileName);
-			lines = Files.readAllLines(path);
-		}
-		catch (Exception e) {}
-	}
+    void readFromFile(String fileName)
+    {
+        try
+        {
+            Path path = Paths.get(".", fileName);
+            lines = Files.readAllLines(path);
+        }
+        catch (Exception e) {}
+    }
 
-	int solve()
-	{
-		int sum = 0;
-		for (String line : lines)		
-		{
-			String[] parts = line.split("\t");
-			int max = 0;
-			int min = Integer.MAX_VALUE;
-			for (int i = 0; i < parts.length; i++)
-			{
-				int number = Integer.parseInt(parts[i]);
-				if (number > max)
-					max = number;
-				if (number < min)
-					min = number;
-			}
-			int difference = max - min;
-			sum += difference;
+    int solvePart1()
+    {
+        int sum = 0;
+        for (String line : lines)
+        {
+            String[] parts = line.split("\t");
+            int max = 0;
+            int min = Integer.MAX_VALUE;
+            for (int i = 0; i < parts.length; i++)
+            {
+                int number = Integer.parseInt(parts[i]);
+                if (number > max)
+                    max = number;
+                if (number < min)
+                    min = number;
+            }
+            int difference = max - min;
+            sum += difference;
+        }
+        return sum;
+    }
 
-		}
-		return sum;
-	}
+    int solvePart2()
+    {
+        int sum = 0;
+        for (String line : lines)
+        {
+            String[] parts = line.split("\t");
+            int solution = 0;
+            for (int i = 0; i < parts.length; i++)
+            {
+                int first = Integer.parseInt(parts[i]);
+                for (int j = 0; j < parts.length; j++)
+                {
+                    int second = Integer.parseInt(parts[j]);
+                    if (first != second)
+                    {
+                        int bigger = Math.max(first, second);
+                        int smaller = Math.min(first, second);
+                        if (bigger % smaller == 0)
+                            solution = bigger / smaller;
+                    }
+                }
+            }
+            sum += solution;
+        }
+        return sum;
+    }
 }
 
 class Day2
 {
-	public static void main(String[] args)
-	{
-		if (args.length < 1) 
-		{
-			System.out.println("Usage: java Day2 fileName");
-			System.exit(0);
-		}
+    public static void main(String[] args)
+    {
+        if (args.length < 1)
+        {
+            System.out.println("Usage: java Day2 fileName");
+            System.exit(0);
+        }
 
-		String fileName = args[0]; 
-		SpreadSheet spreadSheet = new SpreadSheet();
-		spreadSheet.readFromFile(fileName);
-		int solution = spreadSheet.solve();
-		System.out.println("Solution is " + solution);		
-	}
+        String fileName = args[0];
+        SpreadSheet spreadSheet = new SpreadSheet();
+        spreadSheet.readFromFile(fileName);
+        int solution1 = spreadSheet.solvePart1();
+        System.out.println("Part 1 solution is " + solution1);
+        int solution2 = spreadSheet.solvePart2();
+        System.out.println("Part 2 solution is " + solution2);
+    }
 }
